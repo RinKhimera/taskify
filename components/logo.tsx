c24e755c011e3df974aeb6fcfc59b28527845caf
Samuel Pokam <119413624+RinKhimera@users.noreply.github.com>
2023-11-15 19:54:43 +0100

    organization page

--- components/logo.tsx
+++ components/logo.tsx
@@ -11,7 +11,7 @@ export const Logo = () => {
   return (
     <Link href="/">
       <div className="hidden items-center gap-x-2 transition hover:opacity-75 md:flex">
-        <Image src="logo.svg" alt="Logo" height={30} width={30} />
+        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
         <p className={cn("text-lg text-neutral-700", headingFont.className)}>
           Taskify
         </p>
