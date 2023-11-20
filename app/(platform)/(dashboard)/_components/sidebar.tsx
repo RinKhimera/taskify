1a6e7d263df165f35e4e971375129b05b3036fb1
Samuel Pokam <119413624+RinKhimera@users.noreply.github.com>
2023-11-20 12:37:39 +0100

    Sidebar imp

---
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { Accordion } from "@/components/ui/accordion"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import { Skeleton } from "@/components/ui/skeleton"
+import { useOrganization, useOrganizationList } from "@clerk/nextjs"
+import { Plus } from "lucide-react"
+import Link from "next/link"
+import { useLocalStorage } from "usehooks-ts"
+
+type SidebarProps = {
+  storageKey?: string
+}
+
+export const Sidebar = ({ storageKey }: SidebarProps) => {
+  return <div>Sidebar!</div>
+}
